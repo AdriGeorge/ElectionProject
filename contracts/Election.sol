b4a5fd1a6380b9db2ca6c1e43f6bb5eeb24cc5c3
AlessandroPallante-consoft <71274496+AlessandroPallante-consoft@users.noreply.github.com>
2021-10-19 12:24:39 +0200

    Update Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -3,19 +3,31 @@ pragma solidity >=0.8.0;
 // We have to specify what version of compiler this code will compile with
 
 contract Election {
-    
+
+    /*lo si mette nel costruttore per dare una proprietà allo smart contract, 
+    in modo da poter compiere alcune azione precise, come ad esempio 
+    resettare la votazione o remunerare i vincitori
+    */
     address private owner;
+    
+    //numero di voti per poter conscludere la votazione
     uint256 public maxVotes;
+    
+    //numero totali di votazioni effettuate
     uint256 public totalVotes;
     
     /* mapping field below is equivalent to an associative array or hash.
-    The key of the mapping is candidate name stored as type bytes32 and value is
+    The key of the mapping is candidate name stored as type address and value is
     an unsigned integer to store the vote count
     */
     mapping (address => uint256) public votesReceived;
-      
+    
+    //lista dei candidati alla votazioni
     address[] public candidateList;
+    
+    //lista dei vincitori, chi ha ricevuto più voti
     address payable[] public winners;
+    
     address[] private voterList;
     
     // events
@@ -72,7 +84,8 @@ contract Election {
     }
     
     // Reset the election
-    function reset() public {
+    function reset() public onlyAdmin {
+    //una mappa non puo essere eliminata, ma solo "pulita" iterando
         for(uint i=0; i<candidateList.length; i++){
             votesReceived[candidateList[i]] = 0;
         }
@@ -100,6 +113,7 @@ contract Election {
         return voteToWin;
     } 
     
+    /*
     // This function check if an address can vote
     function validVoter(address voter) private view returns (bool) {
         for(uint i = 0; i < voterList.length; i++) {
@@ -109,6 +123,7 @@ contract Election {
         }
         return true;
     }
+    */
         
     // This function check if an address is a candidate
     function validCandidate(address candidate) private view returns (bool) {
